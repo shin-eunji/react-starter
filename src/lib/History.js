678870386b9c666efedc6f2547206a8ccaa993b4
shineunji <dmswlckato@gmail.com>
2020-09-21 12:58:40 +0900

    history edited

--- src/lib/History.js
+++ src/lib/History.js
@@ -1,10 +1,8 @@
 import {createBrowserHistory} from 'history';
 
-const HISTORY = createBrowserHistory();
+export const HISTORY = createBrowserHistory();
 
 
-const navigate = (url) => {
+export const navigate = (url) => {
     HISTORY.push(url)
 };
-
-export {HISTORY, navigate};
