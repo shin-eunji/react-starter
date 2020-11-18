a2267b56daa8a7ad02443c27ccca3ead5fc4c820
shineunji <dmswlckato@gmail.com>
2020-11-18 10:45:34 +0900

    remote

--- src/redux/actionCreators.js
+++ src/redux/actionCreators.js
@@ -1,12 +1,10 @@
 import store from './store'
 import {bindActionCreators} from "redux";
 
-import {appAction} from './app/redux'
+import {Action as appAction} from './app/redux';
 
 const dispatch = store.dispatch;
 
-const appActions = bindActionCreators(appAction.Creators.updateState, dispatch);
-
-
+export const appActions = bindActionCreators(appAction.Creators, dispatch);
 
 export default appActions;
